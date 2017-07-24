cb48c128e74170e560f5c784b05ca8ab4edcb771
Stefan <Stefan.Murga@hotmail.com>
2017-07-24 18:30:56 -0400

    .

---
+++ math-cv/explore_data.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import misc
+import os
+
+
+def tokenized_formula_lengths():
+    f = open('dataset/normalized_formulas.lst').readlines()
+    formula_lengths = [len(x) for x in f]
+    return formula_lengths
+
+
+def crop_image(img):
+    boundary = np.where(img != 255)
+    if np.count_nonzero(boundary) > 0:
+        return np.max(boundary[0]) - np.min(boundary[0]), np.max(boundary[1]) - np.min(boundary[1])
+    return None
+
+
+def cropped_image_sizes():
+    width = []
+    height = []
+    for image_file in os.listdir('dataset/formula_images/'):
+        if image_file == '.DS_Store':
+            continue
+        original_image = np.asarray(misc.imread("dataset/formula_images/" + image_file, True))
+        cropped_image = crop_image(original_image)
+        if cropped_image is not None:
+            width.append(cropped_image[0])
+            height.append(cropped_image[1])
+
+    return width, height
+
+
+
+def plot_histograms(datasets):
+    for dataset in datasets:
+        plt.figure(datasets.index(dataset))
+        plt.hist(dataset[0])
+        plt.title(dataset[1])
+        plt.savefig('dataset/' + dataset[1] + '.png')
+
+
+if __name__ == '__main__':
+    histogram_datasets = []
+    histogram_datasets.append((tokenized_formula_lengths(),'formula_lengths'))
+    images_sizes = cropped_image_sizes()
+    histogram_datasets.append((images_sizes[0], 'image_heights'))
+    histogram_datasets.append((images_sizes[1], 'image_widths'))
+    plot_histograms(histogram_datasets)
+
--- math-cv/preprocess.py
+++ math-cv/preprocess.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy import misc
 import re
 
-DATASET = "../dataset/"
+DATASET = "dataset/"
 TARGET = DATASET + "target/"
 
 width = []
@@ -34,10 +34,10 @@ def crop_image(img):
 
 
 def prepare_im2latex_dataset():
-    training_file = open(IM_2_LATEX_DATASET_PATH + "im2latex_train.lst").readlines()
+    training_file = open(DATASET + "im2latex_train.lst").readlines()
     training_image_file_names = [x.split()[1] for x in training_file]
     for x in range(10000):
-        img = np.asarray(misc.imread(IM_2_LATEX_DATASET_PATH + "formula_images/" + training_image_file_names[x] + ".png", True))
+        img = np.asarray(misc.imread(DATASET + "formula_images/" + training_image_file_names[x] + ".png", True))
         crop_image(img)
         print x
 
@@ -48,6 +48,7 @@ def show_img(img):
 
 
 def clean_formula(formula):
+    formula = formula[:-1]
     # Uncomment line
     if formula[0] == "%":
         formula = formula[1:]
@@ -56,10 +57,10 @@ def clean_formula(formula):
     formula = formula.replace("\\>", "")
     formula = formula.replace("\\;", "")
     formula = formula.replace("\\rm", "")
+    formula = formula.replace("\\ ", "")
     formula = formula.replace(" ", "")                  # Spaces
     formula = formula.replace("\t", "")                  # Tabs
     formula = re.sub(r'\\label{.*?}', "", formula)
-    formula = formula[:-1]
     formula = formula + "\\eos"
     return formula
 
@@ -68,15 +69,13 @@ def prepare_formulas():
     formulas = open(DATASET + "im2latex_formulas.lst").readlines()
     vocabulary = open(DATASET + "latex_vocab.lst").readlines()
 
-    cleaned_formulas = [clean_formula(x) for x in formulas[:1000]]
+    cleaned_formulas = [clean_formula(x) for x in formulas[:100000]]
     mapper = [(x[:-1], vocabulary.index(x)) for x in vocabulary]
 
     integerized_formulas = []
     tokenized_formulas = []
     bad_strings = []
     for formula in cleaned_formulas:
-        if cleaned_formulas.index(formula) % 1000 == 0:
-            print cleaned_formulas.index(formula)
         str_formula = formula
         str_old = ""
         int_formula = ""
@@ -89,7 +88,7 @@ def prepare_formulas():
             for vocab, mapping in mapper:
                 if str_formula.startswith(vocab):
                     int_formula = int_formula + str(mapping) + " "
-                    tokenized_formula = tokenized_formula + vocab
+                    tokenized_formula = tokenized_formula + vocab + " "
                     str_formula = str_formula[len(vocab):]
                     break
         int_formula = int_formula[:-1]
@@ -97,16 +96,13 @@ def prepare_formulas():
         tokenized_formulas.append(tokenized_formula)
 
     outfile = open(TARGET + "integerized_formulas.txt", "w")
-    for x in integerized_formulas:
-        outfile.write(x)
+    outfile.write("\n".join(integerized_formulas))
 
     outfile = open(TARGET + "tokenized_formulas.txt", "w")
-    for x in tokenized_formulas:
-        outfile.write(x)
+    outfile.write("\n".join(tokenized_formulas))
 
     outfile = open(TARGET + "bad_formulas.txt", "w")
-    for x in bad_strings:
-        outfile.write(x)
+    outfile.write("\n".join(bad_strings))
 
 
 if __name__ == "__main__":
