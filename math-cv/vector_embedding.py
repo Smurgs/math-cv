d795252d214268f736b733059ad7beaeaa2b9982
Stefan <Stefan.Murga@hotmail.com>
2017-08-05 17:12:00 -0400

    trained embeddings

--- math-cv/vector_embedding.py
+++ math-cv/vector_embedding.py
@@ -118,24 +118,24 @@ def generate_batch(label):
     return data, labels
 
 
-def plot_with_labels(low_dim_embs, labels, filename='tsne.png'):
-  assert low_dim_embs.shape[0] >= len(labels), 'More labels than embeddings'
-  plt.figure(figsize=(18, 18))  # in inches
-  for i, label in enumerate(labels):
-    x, y = low_dim_embs[i, :]
-    plt.scatter(x, y)
-    plt.annotate(label,
+def plot_with_labels(low_dim_embs, labels, filename='target/tsne.png'):
+    assert low_dim_embs.shape[0] >= len(labels), 'More labels than embeddings'
+    plt.figure(figsize=(18, 18))  # in inches
+    for i, label in enumerate(labels):
+        x, y = low_dim_embs[i, :]
+        plt.scatter(x, y)
+        plt.annotate(label,
                  xy=(x, y),
                  xytext=(5, 2),
                  textcoords='offset points',
                  ha='right',
                  va='bottom')
 
-  plt.savefig(filename)
+    plt.savefig(filename)
 
 
 # Hyper parameters
-num_steps = 1000
+num_steps = 500
 valid_size = 16
 valid_window = 100
 skip_window = 1
@@ -201,19 +201,16 @@ def main(args):
         optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
         norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
         normalized_embeddings = embeddings / norm
-        valid_embeddings = tf.nn.embedding_lookup(
-            normalized_embeddings, valid_dataset)
-        similarity = tf.matmul(
-            valid_embeddings, normalized_embeddings, transpose_b=True)
         init = tf.global_variables_initializer()
 
+    # Training
+    logging.info('Training...')
     with tf.Session(graph=graph) as session:
         # We must initialize all variables before we use them.
         init.run()
-        print('Initialized')
 
         average_loss = 0
-        for step in xrange(num_steps):
+        for step in range(len(transformed_labels)):
             batch_inputs, batch_labels = generate_batch(transformed_labels[step])
             feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
 
@@ -222,34 +219,24 @@ def main(args):
             _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
             average_loss += loss_val
 
-            if step % 100 == 0:
+            if step % 10000 == 0:
                 if step > 0:
-                    average_loss /= 100
-                # The average loss is an estimate of the loss over the last 100 batches.
-                print('Average loss at step ', step, ': ', average_loss)
+                    average_loss /= 10000
+                # The average loss is an estimate of the loss over the last 10000 batches.
+                logging.info('Average loss at step ' + str(step) + ': ' + str(average_loss))
                 average_loss = 0
 
-            # Note that this is expensive (~20% slowdown if computed every 500 steps)
-            if step % 10000 == 0:
-                sim = similarity.eval()
-                for i in xrange(valid_size):
-                    valid_word = reverse_dictionary[valid_examples[i]]
-                    top_k = 8  # number of nearest neighbors
-                    nearest = (-sim[i, :]).argsort()[1:top_k + 1]
-                    log_str = 'Nearest to %s:' % valid_word
-                    for k in xrange(top_k):
-                        close_word = reverse_dictionary[nearest[k]]
-                        log_str = '%s %s,' % (log_str, close_word)
-                    print(log_str)
         final_embeddings = normalized_embeddings.eval()
+        np.save('target/vector_embeddings.npy', final_embeddings)
 
+    # Visualize
+    logging.info('Generating visualization...')
     tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
     plot_only = 500
     low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
     labels = [reverse_dictionary[i] for i in xrange(plot_only)]
     plot_with_labels(low_dim_embs, labels)
 
-    print 'All done!'
 
 if __name__ == '__main__':
     main(sys.argv[1:])
