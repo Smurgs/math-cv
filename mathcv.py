55abd81d8f3a8b6b6f6201d636a6b2fb478baca0
Stefan <Stefan.Murga@hotmail.com>
2017-10-09 11:30:14 -0400

    add dataset download and fix up data handler

--- mathcv.py
+++ mathcv.py
@@ -1,16 +1,14 @@
 import os
 import time
-import numpy as np
+import subprocess
 import tensorflow as tf
 
 import mathcv.model
 import mathcv.preprocess
 import mathcv.data_handler
-
 from mathcv.config import config
 
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_integer('vocab_size', 563, 'vocabulary size')
 tf.app.flags.DEFINE_integer('learning_rate', 0.1, 'learning rate')
 tf.app.flags.DEFINE_integer('decoder_memory_dim', 512, 'decoder lstm memory size')
 tf.app.flags.DEFINE_float('dropout_prob', 0.75, 'dropout probability')
@@ -24,12 +22,43 @@ tf.app.flags.DEFINE_float('dropout_prob', 0.75, 'dropout probability')
 
 
 def main(argv):
-    print 'Starting mathcv'
-    preprocess()
+    print ('MathCv v0.0.1')
+    if len(argv) <= 1:
+        print_usage()
+    elif argv[1] == 'download':
+        download_dataset()
+    elif argv[1] == 'preprocess':
+        mathcv.preprocess.preprocess_dataset()
+    elif argv[1] == 'train':
+        train_model()
+    else:
+        print_usage()
 
 
-def preprocess():
-    mathcv.preprocess.preprocess_dataset()
+def print_usage():
+    print ('Usage: mathcv command')
+    print ('Commands:')
+    print ('    download            download the im2latex dataset')
+    print ('    preprocess          preprocess the train/val/test dataset')
+    print ('    train               train the mathcv model')
+
+
+def download_dataset():
+    dataset_dir = os.path.join(config['root_dir'], 'dataset')
+    if not os.path.exists(dataset_dir):
+        os.makedirs(dataset_dir)
+    os.chdir(dataset_dir)
+    print ('Downloading formulas')
+    subprocess.call('curl -O https://zenodo.org/record/56198/files/im2latex_formulas.lst', shell=True)
+    print ('Downloading dataset partitions')
+    subprocess.call('curl -O https://zenodo.org/record/56198/files/im2latex_train.lst', shell=True)
+    subprocess.call('curl -O https://zenodo.org/record/56198/files/im2latex_validate.lst', shell=True)
+    subprocess.call('curl -O https://zenodo.org/record/56198/files/im2latex_test.lst', shell=True)
+    print ('Downloading images')
+    subprocess.call('curl -O https://zenodo.org/record/56198/files/formula_images.tar.gz', shell=True)
+    print ('Extracting images from archive')
+    subprocess.call('tar -xzvf formula_images.tar.gz', shell=True)
+    os.remove('formula_images.tar.gz')
 
 
 def train_model():
@@ -41,8 +70,8 @@ def train_model():
     label_input = tf.placeholder(tf.int64, [None, config['label_length']])
     hp = {
         'label_length': config['label_length'],
+        'vocab_size': data_loader.get_vocab_size(),
         'decoder_memory_dim': FLAGS.decoder_memory_dim,
-        'vocab_size': FLAGS.vocab_size,
         'dropout_prob': FLAGS.dropout_prob,
         'learning_rate': FLAGS.learning_rate
     }
--- mathcv/data_handler.py
+++ mathcv/data_handler.py
@@ -1,3 +1,4 @@
+import json
 import numpy as np
 from PIL import Image
 
@@ -9,8 +10,8 @@ class DataLoader:
         self._train = None
         self._val = None
         self._test = None
-        self._dictionary = None
         self.load_data()
+        self._dictionary = json.loads(open(config['mapper_path'], 'r').read())
 
     def get_train_batches(self):
         return self._train
@@ -22,7 +23,7 @@ class DataLoader:
         return self._test
 
     def get_vocab_size(self):
-        pass
+        return len(self._dictionary)
 
     def load_data(self):
         formulas = open(config['formula_path']).read().split('\n')
@@ -30,63 +31,34 @@ class DataLoader:
         val = open(config['val_path']).read().split('\n')[:-1]
         test = open(config['test_path']).read().split('\n')[:-1]
 
-        train = combine_x_and_y(train[:10], formulas)
-        val = combine_x_and_y(val[:10], formulas)
-        test = combine_x_and_y(test[:10], formulas)
+        train = self.combine_x_and_y(train[:10], formulas)
+        val = self.combine_x_and_y(val[:10], formulas)
+        test = self.combine_x_and_y(test[:10], formulas)
 
-        dictionary = dict()
-        dictionary['\\<start>'] = len(dictionary)
-        dictionary['\\<end>'] = len(dictionary)
-        dictionary['\\<pad>'] = len(dictionary)
-        dictionary['\\<unknown>'] = len(dictionary)
+        self._train = self.batchify(train)
+        self._val = self.batchify(val)
+        self._test = self.batchify(test)
 
-        train = token_to_int(train, dictionary, True)
-        val = token_to_int(val, dictionary)
-        test = token_to_int(test, dictionary)
+    @staticmethod
+    def combine_x_and_y(lst, formulas):
+        combined = []
+        for y in lst:
+            y = y.split(' ')
+            img = np.array(Image.open(config['img_dir'] + y[0]).convert('L'))
+            img = img[..., np.newaxis]
+            formula = json.loads(formulas[int(y[1])])
+            combined.append((img, formula))
+        return combined
 
-        self._train = batchify(train)
-        self._val = batchify(val)
-        self._test = batchify(test)
-        self._dictionary = dictionary
-
-
-def combine_x_and_y(lst, formulas):
-    combined = []
-    for y in lst:
-        y = y.split(' ')
-        img = np.array(Image.open(config['img_dir'] + y[0]).convert('L'))
-        img = img[..., np.newaxis]
-        combined.append((img, formulas[int(y[1])-1]))
-    return combined
-
-
-def token_to_int(data, dictionary, train_data=False):
-    ret = []
-    for unit in data:
-        img, formula = unit
-        formula = formula.split(' ')
-        new_formula = [dictionary['\\<start>']]
-        for token in formula:
-            if token in dictionary:
-                new_formula.append(dictionary[token])
-            elif train_data:
-                dictionary[token] = len(dictionary)
-                new_formula.append(dictionary[token])
-            else:
-                new_formula.append(dictionary['\\<unknown>'])
-
-        new_formula.append(dictionary['\\<end>'])
-        new_formula += (config['label_length'] - len(new_formula)) * [dictionary['\\<pad>']]
-        ret.append((img, new_formula))
-    return ret
+    @staticmethod
+    def batchify(data):
+        batches = []
+        for i in range(0, len(data) - config['batch_size'], config['batch_size']):
+            images = [x[0] for x in data[i:i + config['batch_size']]]
+            labels = [x[1] for x in data[i:i + config['batch_size']]]
+            batches.append((images, labels))
+        return batches
 
 
-def batchify(data):
-    batches = []
-    for i in range(0, len(data) - config['batch_size'], config['batch_size']):
-        images = [x[0] for x in data[i:i + config['batch_size']]]
-
-        labels = [x[1] for x in data[i:i + config['batch_size']]]
-        batches.append((images, labels))
-
-    return batches
+if __name__ == '__main__':
+    dl = DataLoader()
